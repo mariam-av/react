4d1b7bfc0da45a957a31128905dfe1f1303824c8
mariam-av <40037245+mariam-av@users.noreply.github.com>
2019-02-16 13:35:03 +0400

    FormsApp

---
+++ src/components/FormsApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class FormsApp extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default FormsApp;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./components/DataFetch";
+import App from "./components/FormsApp";
 
 ReactDom.render(<App />, document.getElementById("root"));
